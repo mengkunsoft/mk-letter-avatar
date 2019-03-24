46097eb37c44cee25685d845c2b4a512b4bb0935
mengkunsoft <1163540807@qq.com>
2019-03-24 20:26:09 +0800

    走过路过不要错过！帮忙点个 Star 吧~

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# mk-letter-avatar
+mk-letter-avatar
+========
+
+![](https://ws1.sinaimg.cn/large/006Xzox4ly1g1e591lzxzj31h30daaei.jpg)
+
+mk-letter-avatar 是一个简单好用的 WordPress 字母头像插件。
+
+WordPress 默认采用的是 Gravatar 头像，但很多人可能没有设置 Gravatar 头像，因而只能显示出默认的头像。本插件就是将无头像用户的头像显示成动态生成的`昵称首字符`头像。
+
+
+说道字母头像，这里不得不提一款很出名的插件：[WP First Letter Avatar](https://wordpress.org/plugins/wp-first-letter-avatar/)，本插件与之不同的是字母头像的生成完全是在`前端`实现的，整个插件大小只有不到 5kb。插件启用后不会在服务器产生任何缓存文件，纯净好用！
+
+
+本插件无需任何设置，直接在 WordPress 后台上传并启用即可。
+
+插件的头像生成代码基于 https://github.com/daolavi/LetterAvatar 和 https://gist.github.com/leecrossley/6027780
+
+### 在线演示
+-----
+
+参考 [孟坤博客](https://mkblog.cn) 的评论区头像
+
+### 常见问题
+-----
+
+#### 启用插件后不生效
+
+请检查所用的主题是否使用的 WordPress 自带的评论模板或者使用 WordPress 默认函数 `get_avatar()` 获取头像，如若不是，请改用 `get_avatar()`
+
+### 相关推荐
+-----
+[mk-sitemap 站点地图插件](https://github.com/mengkunsoft/mk-sitemap)
+
+[mkBlog 简约主题](https://mkblog.cn/theme-mkblog/)
+
+### 更新日志
+-----
+
+#### v1.0.0 `2019/3/24`
+- 横空出世！
---
+++ mk-letter-avatar.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Plugin Name: mk-letter-avatar
+ * Plugin URI: https://mkblog.cn/
+ * Description: 孟坤字母头像插件，如果评论者没有设置 Gravatar 头像，自动使用昵称首个字符作为头像
+ * Version: 1.0.0
+ * Author: mengkun
+ * Author URI: https://mkblog.cn/
+ */
+
+if(!function_exists('mk_letter_avatar')) {
+    
+    /**
+     * 生成头像的颜色列表
+     * 
+     * @notice 这里的颜色值 *必须* 是 20 个！
+     */
+    $AVATAR_COLORS = array(
+        '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50', 
+        '#f1c40f', '#e67e22', '#e74c3c', '#00bcd4', '#95a5a6', '#f39c12', '#d35400', '#c0392b', '#bdc3c7', '#7f8c8d'
+        );
+    
+    /**
+     * 挂载字母头像替换
+     */
+    function mk_letter_avatar( $avatar, $id_or_email = '', $size = '40', $default = '', $alt = '' ) { 
+        // 匹配出原始头像中的信息
+        $src   = mk_get_html_tag_attribute( $avatar, 'src' );
+        if(!$src) return $avatar;
+        
+        // 去除原始头像链接中的默认头像，改为输出 404
+        $src   = preg_replace('/[\?\&]d[^&]+/is', '&d=404', $src);
+        
+        $class = mk_get_html_tag_attribute( $avatar, 'class', 'avatar avatar-'.$size.' photo' );
+        $alt   = mk_get_html_tag_attribute( $avatar, 'alt',   $alt );
+        $title = mk_get_html_tag_attribute( $avatar, 'title', $alt );
+        
+        $avatar = '<img src="'.$src.'" alt="'.$alt.'" title="'.$title.'" class="'.$class.' mk-letter-avatar" height="'.$size.'" width="'.$size.'" onerror="onerror=null;src=\'\';src=mkLetterAvatar(alt,'.$size.')" />';
+        return $avatar;
+    }
+    add_filter( 'get_avatar', 'mk_letter_avatar', 999999, 5 );
+    
+    /**
+     * 挂载字母头像生成 js 文件
+     */
+    function mk_letter_avatar_js() {
+        global $AVATAR_COLORS;
+        /*
+         * LetterAvatar   https://github.com/daolavi/LetterAvatar
+         * 
+         * Artur Heinze
+         * Create Letter avatar based on Initials
+         * based on https://gist.github.com/leecrossley/6027780
+         */
+        ?><style>.mk-letter-avatar[src=""]{visibility: hidden;}</style><script>(function(e,g){window.mkLetterAvatar=function(a,b,f){b=b||60;var h="<?php echo implode(' ', $AVATAR_COLORS); ?>".split(" ");a=String(a||"").split(" ");a=1==a.length?a[0]?a[0].charAt(0):"?":a[0].charAt(0)+a[1].charAt(0);e.devicePixelRatio&&(b*=e.devicePixelRatio);var k=(("?"==a?72:a.charCodeAt(0))-64)%20;var c=g.createElement("canvas");c.width=b;c.height=b;var d=c.getContext("2d");d.fillStyle=
+f?f:h[k-1];d.fillRect(0,0,c.width,c.height);d.font=Math.round(c.width/2)+"px 'Microsoft Yahei'";d.textAlign="center";d.fillStyle="#FFF";d.fillText(a,b/2,b/1.5);return c.toDataURL()}})(window,document);</script><?php
+    }
+    add_action( 'wp_head', 'mk_letter_avatar_js' );
+    
+    /**
+     * 获取 Html 标签内的指定属性值
+     * 
+     * @param $html      原始 html 标签代码
+     * @param $attribute 要获取的标签
+     * @param $default   如果没有获取到，返回的默认值
+     * @return result
+     */
+    function mk_get_html_tag_attribute( $html, $attribute, $default = '' ) {
+        if(preg_match('/'.$attribute.'\s*=\s*[\"\']([^\"\']*)[\"\']/isU', $html, $result)) {
+            if(isset($result[1])) return $result[1];
+        }
+        return $default;
+    }
+}
