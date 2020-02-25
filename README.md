ead011b565957df530ef487c02b18c3146b39c05
mengkun <1163540807@qq.com>
2020-02-25 21:15:30 +0800

    优化代码，适配更多主题
    - 优化代码，适配更多主题
    - 支持在后台也显示字母头像

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 mk-letter-avatar
 ========
 
-![](https://ws1.sinaimg.cn/large/006Xzox4ly1g1e591lzxzj31h30daaei.jpg)
+![](https://user-images.githubusercontent.com/16880885/75250417-498fb880-5813-11ea-96ed-1ba158e9e67e.png)
 
 [![GitHub stars](https://img.shields.io/github/stars/mengkunsoft/mk-letter-avatar.svg)](https://github.com/mengkunsoft/mk-letter-avatar/stargazers) [![GitHub forks](https://img.shields.io/github/forks/mengkunsoft/mk-letter-avatar.svg)](https://github.com/mengkunsoft/mk-letter-avatar/network) [![GitHub issues](https://img.shields.io/github/issues/mengkunsoft/mk-letter-avatar.svg)](https://github.com/mengkunsoft/mk-letter-avatar/issues) [![GitHub license](https://img.shields.io/github/license/mengkunsoft/mk-letter-avatar.svg)](https://github.com/mengkunsoft/mk-letter-avatar/blob/master/LICENSE)
 
@@ -9,9 +9,7 @@ mk-letter-avatar 是一个简单好用的 WordPress 字母头像插件。
 
 WordPress 默认采用的是 Gravatar 头像，但很多人可能没有设置 Gravatar 头像，因而只能显示出默认的头像。本插件就是将无头像用户的头像显示成动态生成的`昵称首字符`头像。
 
-
-说到字母头像，这里不得不提一款很出名的插件：[WP First Letter Avatar](https://wordpress.org/plugins/wp-first-letter-avatar/)，本插件与之不同的是字母头像的生成完全是在`前端`实现的，整个插件大小只有不到 5kb。插件启用后不会在服务器产生任何缓存文件，纯净好用！
-
+说到字母头像，其实之前已有多种方案。本插件与其它同类插件不同的是字母头像的生成完全是在`前端`实现的，而且支持中文。整个插件大小只有 5kb 左右。插件启用后不会在服务器产生任何缓存文件，纯净好用！
 
 本插件无需任何设置，直接在 WordPress 后台上传并启用即可。
 
@@ -27,7 +25,11 @@ WordPress 默认采用的是 Gravatar 头像，但很多人可能没有设置 Gr
 
 #### 启用插件后不生效
 
-请检查所用的主题是否使用的 WordPress 自带的评论模板或者使用 WordPress 默认函数 `get_avatar()` 获取头像，如若不是，请改用 `get_avatar()`
+请检查所用的主题是否使用的 WordPress 自带的评论模板或者使用 WordPress 默认函数 `get_avatar()` 获取头像，如若不是，请改用 `get_avatar()`。
+
+#### 启用插件后控制台多了很多 404 报错
+
+因为插件需要根据 Gravatar 返回的状态来判断是否有头像，所以会产生这样的问题。除此之外，无任何“毒副作用”。如果觉得看着难受，可以在页面加载完成后使用 `console.clear();` 清除控制台的错误输出。
 
 ### 相关推荐
 -----
@@ -39,6 +41,10 @@ WordPress 默认采用的是 Gravatar 头像，但很多人可能没有设置 Gr
 
 ### 更新日志
 -----
+
+#### v1.1.0 `2020/2/25`
+- 优化代码，适配更多主题
+- 支持在后台也显示字母头像
 
 #### v1.0.4 `2019/4/27`
 - 修改为仅绘制一个字符作为头像
--- mk-letter-avatar.php
+++ mk-letter-avatar.php
@@ -3,7 +3,7 @@
  * Plugin Name: mk-letter-avatar
  * Plugin URI: https://github.com/mengkunsoft/mk-letter-avatar
  * Description: 孟坤字母头像插件，如果评论者没有设置 Gravatar 头像，自动使用昵称首个字符作为头像
- * Version: 1.0.4
+ * Version: 1.1.0
  * Author: mengkun
  * Author URI: https://mkblog.cn/
  */
@@ -16,31 +16,72 @@ if(!function_exists('mk_letter_avatar')) {
     $AVATAR_COLORS = array(
         '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50', 
         '#f1c40f', '#e67e22', '#e74c3c', '#00bcd4', '#95a5a6', '#f39c12', '#d35400', '#c0392b', '#bdc3c7', '#7f8c8d'
-        );
+    );
     
     /**
      * 挂载字母头像替换
      */
-    function mk_letter_avatar( $avatar, $id_or_email = '', $size = '40', $default = '', $alt = '' ) { 
+    function mk_letter_avatar($avatar, $id_or_email = '', $size = '40', $default = '', $alt = '') { 
         // 匹配出原始头像中的信息
-        $src = mk_get_html_tag_attribute( $avatar, 'src' );
+        $src = mk_get_html_tag_attribute($avatar, 'src');
         if(!$src) return $avatar;
         
-        $alt = mk_get_html_tag_attribute( $avatar, 'alt',   $alt );
+        $alt = mk_get_avatar_name($id_or_email);
+        $alt = $alt? $alt: mk_get_html_tag_attribute($avatar, 'alt', $alt);
         if(!$alt) return $avatar;
         
         // 去除原始头像链接中的默认头像，改为输出 404
         $src = htmlspecialchars_decode($src);
-        $src = preg_replace('/[\?\&]d[^&]+/is', '&d=404', $src);
+        $src = preg_replace('/[\?\&]d[^&]+/is', '', $src);
+        $src = $src.'&d=404';
         $src = htmlspecialchars($src);
         
-        $class = mk_get_html_tag_attribute( $avatar, 'class', 'avatar avatar-'.$size.' photo' );
-        $title = mk_get_html_tag_attribute( $avatar, 'title', $alt );
+        $class = mk_get_html_tag_attribute($avatar, 'class', 'avatar avatar-'.$size.' photo');
+        $title = mk_get_html_tag_attribute($avatar, 'title', $alt);
         
         $avatar = '<img src="'.$src.'" alt="'.$alt.'" title="'.$title.'" class="'.$class.' mk-letter-avatar" height="'.$size.'" width="'.$size.'" onerror="onerror=null;src=\'\';src=mkLetterAvatar(alt,'.$size.')" />';
         return $avatar;
     }
-    add_filter( 'get_avatar', 'mk_letter_avatar', 999999, 5 );
+    global $pagenow;
+    if(!is_admin() || $pagenow != 'options-discussion.php') {    // 排除后台“讨论设置”的头像
+        add_filter('get_avatar', 'mk_letter_avatar', 999999, 5);
+	}
+    
+    /**
+     * 取出头像对应的用户名
+     */ 
+    function mk_get_avatar_name($id_or_email) {
+        if(have_comments()) {
+            return get_comment_author();
+        }
+        
+        $user = null;
+        if(empty($id_or_email)) {
+            return null;
+        } else if(is_object($id_or_email)) {
+            if(!empty($id_or_email->comment_author)) {
+                return $id_or_email->comment_author;
+            } else if(!empty($id_or_email->user_id)) {
+                $id = (int) $id_or_email->user_id;
+                $user = get_user_by('id', $id);
+            }
+        } else if(is_numeric($id_or_email)) { // 是数字，尝试获取该 ID 对应的用户名
+            $id = (int) $id_or_email;
+            $user = get_user_by('id', $id);
+        } else if(is_string($id_or_email)) {
+            if (!filter_var($id_or_email, FILTER_VALIDATE_EMAIL)) { // 不是邮箱，当做用户名
+                return $id_or_email;
+            } else {
+                $user = get_user_by('email', $email);
+            }
+        }
+        // 尝试从用户对象中取出用户名
+        if(!empty($user) && is_object($user)) {
+            return $user->display_name;
+        }
+        return null;
+    }
+    
     
     /**
      * 挂载字母头像生成 js 文件
@@ -56,7 +97,8 @@ if(!function_exists('mk_letter_avatar')) {
          */
         ?><style>.mk-letter-avatar[src=""]{visibility: hidden;}</style><script>(function(a,b){window.mkLetterAvatar=function(d,l,j){d=d||"";l=l||60;var h="<?php echo implode(' ', $AVATAR_COLORS); ?>".split(" "),f,c,k,g,e,i;f=String(d);f=f.replace(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g,"");f=f?f.charAt(0):"?";if(a.devicePixelRatio){l=(l*a.devicePixelRatio)}c=(f=="?"?72:f.charCodeAt(0))-64;k=c%h.length;g=b.createElement("canvas");g.width=l;g.height=l;e=g.getContext("2d");e.fillStyle=j?j:h[k-1];e.fillRect(0,0,g.width,g.height);e.font=Math.round(g.width/2)+"px 'Microsoft Yahei'";e.textAlign="center";e.fillStyle="#fff";e.fillText(f,l/2,l/1.5);i=g.toDataURL();g=null;return i}})(window,document);</script><?php
     }
-    add_action( 'wp_head', 'mk_letter_avatar_js' );
+    add_action('wp_head',    'mk_letter_avatar_js');
+    add_action('admin_head', 'mk_letter_avatar_js');
     
     /**
      * 获取 Html 标签内的指定属性值
@@ -66,7 +108,7 @@ if(!function_exists('mk_letter_avatar')) {
      * @param $default   如果没有获取到，返回的默认值
      * @return result
      */
-    function mk_get_html_tag_attribute( $html, $attribute, $default = '' ) {
+    function mk_get_html_tag_attribute($html, $attribute, $default = '') {
         if(preg_match('/'.$attribute.'\s*=\s*[\"\']([^\"\']*)[\"\']/isU', $html, $result)) {
             if(isset($result[1])) return $result[1];
         }
